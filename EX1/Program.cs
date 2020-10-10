6dbc8ee249fe8e5b50598ec5563e0b22260b012a
Ricardo Monteiro <71653983+m0nte1ro@users.noreply.github.com>
2020-10-10 15:36:55 +0100

    Add files via upload

---
+++ EX1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Retangulo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Retangulo r = new Retangulo();
+
+            Console.WriteLine("Insira uma largura: ");
+            r.largura=Int32.Parse(Console.ReadLine());
+            Console.WriteLine("Insira um comprimento: ");
+            r.comprimento=Int32.Parse(Console.ReadLine());
+
+            r.Area();
+            r.Quadrado();
+            r.ToString();
+        }
+    }
+}
---
+++ EX1/Retangulo.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Retangulo
+{
+    class Retangulo
+    {
+        private float _largura, _comprimento;
+        public Retangulo()
+        {
+            _largura = 0;
+            _comprimento = 0;
+        }
+        public Retangulo(float largura, float comprimento)
+        {
+            this._largura = largura;
+            this._comprimento = comprimento;
+        }
+
+        public float largura
+        {
+            get { return _largura; }
+            set
+            {
+                if (value > 20)
+                {
+                    _largura = 20;
+                }
+                else
+                {
+                    _largura = value;
+                }
+            }
+        }
+
+        public float comprimento
+        {
+            get { return _comprimento; }
+            set
+            {
+                if (value > 20)
+                {
+                    _comprimento = 20;
+                }
+                else
+                {
+                    _comprimento = value;
+                }
+            }
+        }
+        /*
+        public float GetLargura()
+        {
+            return largura;
+        }
+        public void SetLargura(float largura)
+        {
+            this.largura = largura;
+        }
+        public float GetComprimento()
+        {
+            return comprimento;
+        }
+        public void SetComprimento(float comprimento)
+        {
+            this.comprimento = comprimento;
+        }
+        */
+        public float Area()
+        {
+            Console.WriteLine(_largura * _comprimento);
+            return _largura * _comprimento;
+        }
+
+        public bool Quadrado()
+        {
+            if (_largura == _comprimento)
+            {
+                Console.WriteLine("É um quadrado");
+                return true;
+            }
+            else
+            {
+                Console.WriteLine("Não é um quadrado");
+                return false;
+            }
+        }
+
+        public string ToString()
+        {
+            Console.WriteLine("O retangulo mede: " + _largura + "cm * " + _comprimento + "cm.");
+            return "";
+        }
+    }
+}
